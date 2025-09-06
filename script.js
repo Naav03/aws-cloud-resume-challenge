aeab83d2b535d82a47474166e45bf5c6fd2c418e
Naav03 <148970944+Naav03@users.noreply.github.com>
2025-09-06 03:24:49 -0400

    Delete script.js

--- script.js
+++
@@ -1,7 +0,0 @@
-
-document.addEventListener('DOMContentLoaded', () => {
-  let count = localStorage.getItem('visitor-count') || 0;
-  count++;
-  localStorage.setItem('visitor-count', count);
-  document.getElementById('counter').textContent = count;
-});
